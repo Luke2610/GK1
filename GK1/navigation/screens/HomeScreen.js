d3645033ca95e4ceeed514d84f0ae7a455e6ccb6
Lucas <luve20ac@student.cbs.dk>
2022-09-28 14:51:58 +0200

    Picture + event format

--- GK1/navigation/screens/HomeScreen.js
+++ GK1/navigation/screens/HomeScreen.js
@@ -3,24 +3,22 @@ import {
   Text,
   View,
   ScrollView,
-  Dimensions,
   FlatList,
+  Image,
 } from 'react-native';
 import React, { useEffect, useState } from 'react';
-import Ionicons from '@expo/vector-icons/Ionicons';
 import { database } from '../../firebase';
 import { ref, onValue } from 'firebase/database';
+import tennis from '../../assets/Tennis_Racket_and_Balls.png';
 
 export default function HomeScreen({ navigation }) {
   const eventsRef = ref(database, '/events');
   const [events, setEvents] = useState({});
 
+  //funktion der kører når applikationen åbnes
   useEffect(() => {
     return onValue(eventsRef, (snapshot) => {
-      let data = snapshot.val() || {};
-      let eventsItems = { ...data };
       setEvents(snapshot.val());
-      console.log(eventsItems);
     });
   }, []);
 
@@ -29,46 +27,68 @@ export default function HomeScreen({ navigation }) {
 
   return (
     <View style={styles.container}>
-      <View style={styles.homePart}>
-        <Text style={styles.headingText}>Recommendation</Text>
-        <FlatList
-          data={eventsArray}
-          horizontal={true}
-          style={styles.homeMini}
-          showsHorizontalScrollIndicator={false}
-          keyExtractor={(item, index) => eventsKeys[index]}
-          renderItem={({ item, index }) => {
-            return (
-              <View style={styles.event}>
-                <Text>Name: {item.name}</Text>
-                <Text>Type: {item.type}</Text>
-                <Text>Place: {item.place}</Text>
-              </View>
-            );
-          }}
-        />
-      </View>
-      <View style={styles.homePart}>
-        <Text style={styles.headingText}>Near You</Text>
-
-        <ScrollView
-          horizontal={true}
-          bounces={true}
-          style={styles.homeMini}
-          showsHorizontalScrollIndicator={false}
-        >
-          <View style={styles.event}></View>
-          <View style={styles.event}></View>
-        </ScrollView>
-      </View>
+      <ScrollView>
+        <View style={styles.homePart}>
+          <Text style={styles.headingText}>Recommendation</Text>
+          <FlatList
+            data={eventsArray}
+            horizontal={true}
+            style={styles.homeMini}
+            showsHorizontalScrollIndicator={false}
+            keyExtractor={(item, index) => eventsKeys[index]}
+            renderItem={({ item, index }) => {
+              return (
+                <View style={styles.event}>
+                  <View style={styles.eventPic}>
+                    <Image
+                      source={tennis}
+                      style={{
+                        alignSelf: 'center',
+                        flex: 1,
+                        width: '100%',
+                        borderRadius: 40,
+                      }}
+                      resizeMode={'cover'}
+                    />
+                  </View>
+                  <View style={styles.eventInfo}>
+                    <Text style={styles.eventHeading}>{item.name}</Text>
+                    <Text>Type: {item.type}</Text>
+                    <Text>Place: {item.place}</Text>
+                  </View>
+                </View>
+              );
+            }}
+          />
+        </View>
+        <View style={styles.homePart}>
+          <Text style={styles.headingText}>Near You</Text>
+          <FlatList
+            data={eventsArray}
+            horizontal={true}
+            style={styles.homeMini}
+            showsHorizontalScrollIndicator={false}
+            keyExtractor={(item, index) => eventsKeys[index]}
+            renderItem={({ item, index }) => {
+              return (
+                <View style={styles.event}>
+                  <Text>Name: {item.name}</Text>
+                  <Text>Type: {item.type}</Text>
+                  <Text>Place: {item.place}</Text>
+                </View>
+              );
+            }}
+          />
+        </View>
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: { flex: 1 },
-  headingText: { fontSize: 22, fontWeight: 'bold' },
-  homePart: { flex: 1, padding: 30, borderWidth: 1 },
+  headingText: { fontSize: 22, fontWeight: 'bold', margin: 5 },
+  homePart: { flex: 1, padding: 30 },
   homeMini: {
     flex: 1,
     padding: 5,
@@ -77,11 +97,26 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     marginLeft: 20,
     marginRight: 20,
-    height: '100%',
-    //width: Dimensions.get('window').width * 0.6, //Sætter bredde til halvdelen af enhedens bredde
+    height: 300,
+    //width: undefined,
     borderColor: '#86A293',
     borderRadius: 40,
     padding: 15,
-    aspectRatio: 1,
+    aspectRatio: 1, //sætter bredden så den er 1:1 ift. højden
+  },
+  eventHeading: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  eventInfo: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 2,
+    //borderWidth: 1,
+  },
+  eventPic: {
+    flex: 2,
+    margin: -5,
+    alignSelf: 'stretch',
   },
 });
